e408e75dd63f9e4381aaed0de1feb8be876d7a4b
André Ferreira <asf@cesar.org.br>
2021-11-11 19:36:57 -0300

    features: Update dashboard

--- src/features/Dashboard/Dashboard.js
+++ src/features/Dashboard/Dashboard.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { Header, CardContainer, SimpleCard } from 'components';
 import styles from './Dashboard.module.css';
 
+const text = 'Project IoT';
+const tempMsd = 1956;
+const tempRef = 50;
+
 const Dashboard = () => {
-  const text = 'Project IoT';
   return (
     <div className={styles.container}>
       <Header title={text} />
       <div className={styles.temp}>
         <div className={styles.info}>
           <CardContainer title="Temperature" />
-          <SimpleCard content="1956 °C" />
+          <SimpleCard>{`${tempMsd} °C`}</SimpleCard>
         </div>
         <div className={styles.info}>
           <CardContainer title="Reference" />
-          <SimpleCard content="50 °C" />
+          <SimpleCard>{`${tempRef} °C`}</SimpleCard>
         </div>
       </div>
     </div>
