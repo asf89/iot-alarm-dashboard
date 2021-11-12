ee3961984e46c1adb294f7cf663f8d2a66e8c2d3
André Ferreira <asf@cesar.org.br>
2021-11-12 19:44:08 -0300

    components: Add Button component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Header } from './Header';
 export { default as CardContainer } from './Cards/CardContainer';
-export { default as SimpleCard } from './Cards/SimpleCard';
+export { default as Card } from './Cards/Card';
 export { default as Modal } from './Modal';
+export { default as Button } from './Button';
